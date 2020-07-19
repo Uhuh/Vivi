43e162befaab2a35d412f0558a69ab75476689ca
Dylan Warren <duwtgb@mst.edu>
2020-07-19 16:54:21 -0500

    Fix warnings mentions

--- commands/mod/warnings.ts
+++ commands/mod/warnings.ts
@@ -11,7 +11,23 @@ const warnings = {
     if (!args.length) {
       return message.reply(`please supply a user id.`);
     }
-    const userWarnings = GET_USER_WARN(args[0]);
+    
+
+    /**
+     * If they mention the user then use that otherwise they should've sent the user id
+     * args.shift() returns the first element and pops it out of the array.
+     */
+    const userId = message.mentions.members?.first()?.id || args.shift();
+
+    if (message.mentions.members?.first()) args.shift();
+
+    if (!userId) {
+      return message.reply(`please provide a user id or mention.`);
+    } else if (Number.isNaN(Number(userId))) {
+      return message.reply(`user ids are numbers. Please try again.`);
+    }
+
+    const userWarnings = GET_USER_WARN(userId);
     const embed = new MessageEmbed();
 
     console.log(userWarnings);
