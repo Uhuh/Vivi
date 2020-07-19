68daaa78e5efee2dacf1036b0c818a89b5faad18
Dylan Warren <duwtgb@mst.edu>
2020-07-18 21:56:09 -0500

    Unbannnn

---
+++ commands/mod/unban.ts
@@ -0,0 +1,36 @@
+import { Message, MessageEmbed } from "discord.js";
+import BowBot from "../../src/bot";
+
+const unban = {
+	desc: 'Unban a user',
+	name: 'unban',
+	args: '<user id> <reason>',
+	type: 'admin',
+	run: (message: Message, args: string[], client: BowBot) => {
+    if (!message.member?.hasPermission('BAN_MEMBERS')) { return message.react('👎') }
+    if (!args.length) {
+      return message.reply(`you forgot some arguements.`)
+    }
+    /**
+     * If they mention the user then use that otherwise they should've sent the user id
+     * args.shift() returns the first element and pops it out of the array.
+     */
+    const userId = message.mentions.members?.first()?.id || args.shift();
+
+    if(message.mentions.members?.first()) args.shift();
+
+    const reason = args.join(' ').trim() === '' ? 'No reason provided.' : args.join(' ').trim();
+
+    message.guild?.members.unban(userId || '')
+      .then(() => {
+        const embed = new MessageEmbed();
+        client.logIssue('Unban', reason, message.author, userId || 'User');
+        embed.setTitle(`**Unbanned** User (<@${userId}>)`);
+        message.channel.send(embed);
+      })
+      .catch(() => message.reply(`I had issues trying to unban them.`));
+    return;
+	}
+}
+
+export default unban;
