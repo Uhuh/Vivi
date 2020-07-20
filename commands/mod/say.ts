6ac539ef0f7fe78e8d0cbd64248a130e57b14036
Dylan Warren <duwtgb@mst.edu>
2020-07-20 17:55:51 -0500

    tslint

--- commands/mod/say.ts
+++ commands/mod/say.ts
@@ -1,12 +1,11 @@
 import { Message } from "discord.js";
-import BowBot from "../../src/bot";
 
 const say = {
 	desc: ">:)",
 	name: "say",
 	args: "",
 	type: "owner",
-	run: (message: Message, args: string[], client: BowBot) => {
+	run: (message: Message, args: string[]) => {
 		if (args.length > 0 && message.member?.hasPermission(["MANAGE_MESSAGES"])) {
 			message.delete();
 			const toSay = args.join(' ');
