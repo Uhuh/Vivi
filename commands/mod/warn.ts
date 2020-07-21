16aab57b4b2699e20f0501f3ed615a4061bafe39
Dylan Warren <duwtgb@mst.edu>
2020-07-21 16:12:11 -0500

    bow bot doesnt increment right oops

--- commands/mod/warn.ts
+++ commands/mod/warn.ts
@@ -32,17 +32,19 @@ const warn = {
 
     if(!userWarnings) userWarnings = [];
 
-    let numWarns = userWarnings.length
+    let numWarns = userWarnings.length;
+
+    ++numWarns;
 
     const reason = args.join(' ').trim() === '' ? 'No reason provided.' : args.join(' ').trim();
-
+    console.log()
     if (numWarns > 3) {
       message.channel.send(`Banned ${user.displayName} for getting more than 3 strikes.`);
       user.ban().catch(() => message.channel.send(`Issues banning user.`));
       SET_WARN(user.id, reason, message.author.id);
       client.logIssue('AutoMod: Ban', `Strike! You're out! **Reason:** ${reason}`, client.user!, user.user)
     } else {
-      message.channel.send(`<@${user.id}> You've been warned for \`${reason}\`. You have ${++numWarns} strike${numWarns > 1 ? 's' : ''} now.`);
+      message.channel.send(`<@${user.id}> You've been warned for \`${reason}\`. You have ${numWarns} strike${numWarns > 1 ? 's' : ''} now.`);
       SET_WARN(user.id, reason, message.author.id);
       client.logIssue('Warn', reason, message.author, user.user);
       user.send(`You have been warned!\n**Reason:** ${reason}`)
