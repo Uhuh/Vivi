1856d67dc2053bfd125981aec751fa6c261024e8
Dylan Warren <duwtgb@mst.edu>
2020-07-23 08:23:49 -0500

    Unused

--- events/message.ts
+++ events/message.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed, TextChannel } from 'discord.js';
+import { Message } from 'discord.js';
 import BowBot from '../src/bot';
 
 const msg = (client: BowBot, message: Message) => {
@@ -22,29 +22,6 @@ const msg = (client: BowBot, message: Message) => {
     } catch(e) {
       message.reply('something went wrong!', e);
       console.error(e);
-    }
-  } else {
-    const suggestionId = '729665823605522493';
-    const popularId = '735710411382456360';
-    if (message.channel.id === suggestionId) {
-      message.react('⭐');
-      const filter = (react: any) => react.emoji.name === '⭐';
-
-      message.awaitReactions(filter, { time: 600000 })
-        .then(collected => {
-          if (collected.size >= 35) {
-            const channel = message.guild?.channels.cache.get(popularId) as TextChannel;
-            const embed = new MessageEmbed();
-            embed.setTitle(`Popular suggestion. ${collected.size}⭐`)
-              .setAuthor(message.author.tag)
-              .setThumbnail(message.author.avatarURL({ dynamic: true }) || '')
-              .setDescription(message.content)
-              .setTimestamp(new Date());
-
-            channel.send(embed);
-          }
-        })
-        .catch(() => console.error(`Issues collecting reactions for popular.`))
     }
   }
 };
