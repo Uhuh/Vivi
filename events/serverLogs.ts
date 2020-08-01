6653a151dc4bfd6e849f343bbdfe2f46675b507c
Dylan Warren <duwtgb@mst.edu>
2020-07-31 21:26:27 -0500

    Wow

--- events/serverLogs.ts
+++ events/serverLogs.ts
@@ -13,9 +13,9 @@ export const UserJoin = (member: Discord.GuildMember | Discord.PartialGuildMembe
     member.roles?.add(muteId)
       .catch(() => console.error(`Couldn't mute the user on join.`));
   } else if(member instanceof Discord.GuildMember) {
-    member.send(
+    /* member.send(
       `Welcome to the Love Letter community!\nPlease wait 10 minutes until I can help you get verified. In the meantime make sure to read the <#732961554005229639> and look at the <#729135006018175077>.`
-    ).catch(() => console.error(`Couldn't DM user welcome message.`));
+    ).catch(() => console.error(`Couldn't DM user welcome message.`)); */
   }
 }
 
