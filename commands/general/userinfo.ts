ff9912e73c8d66f9a0c433ee4e6a72016abcff1c
Dylan Warren <duwtgb@mst.edu>
2020-08-09 21:30:07 -0500

    New user info stats

--- commands/general/userinfo.ts
+++ commands/general/userinfo.ts
@@ -1,5 +1,7 @@
 import { Message, MessageEmbed } from "discord.js";
 import SetsuBot from "../../src/bot";
+import * as moment from 'moment';
+import { GET_USER_WARN } from "../../src/setup_tables";
 
 export default {
   desc: 'Information about user.',
@@ -25,24 +27,29 @@ export default {
     if (!member) return;
 
     const embed = new MessageEmbed();
-
+    const WEEK_OLD = moment().subtract(7, 'days').startOf('day');
+    const warnings = GET_USER_WARN(userId);
+    const activeWarns = warnings.filter(w => !moment.unix(w.date).isBefore(WEEK_OLD)) || [];
+    
     embed.setTitle(`**User Info**`)
       .setColor(7419530)
       .setThumbnail(member.user.avatarURL({dynamic: true}) || "")
       .setFooter(`Replying to: ${message.author.tag}`)
-      .addField(`**DETAILS**`, 
-        `\`\`\`asciidoc
-• Username :: ${member.user.tag}
-• ID       :: ${member.user.id}
-• Created  :: ${member.user.createdAt.toDateString()}
-• Joined   :: ${member.joinedAt?.toDateString()}\`\`\``
-      )
-      .addField(`**STATUS**`, 
-        `\`\`\`asciidoc
-• Type     :: ${member.user.bot ?  "Beepboop, I'm a bot." : "I'm Human."}
-• Presence :: ${member.presence.activities[0]}\`\`\``
-      )
       .setTimestamp(new Date());
+
+    embed
+      .addField('**Created**', member.user.createdAt.toDateString(), true)
+      .addField('**Joined**', member.joinedAt?.toDateString(), true)
+      .addField('**Username**', member.user.tag, true)
+      .addField('**ID**', member.user.id)
+      .addField('Warnings', warnings.length ? 
+        `Total: ${warnings.length} | Active: ${activeWarns.length}\n\nIDs: ${warnings.map(w => w.id)}` :
+        'No warnings for this user'
+      )
+      .addField('Active Warnings', activeWarns.length ? 
+        activeWarns.map(w => `ID: ${w.id} | Reason: ${w.reason}\n`).join('') :
+        'No active warnings for this user'
+      );
 
     message.channel.send({ embed })
   }
