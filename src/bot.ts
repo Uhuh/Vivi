d3ce9babc64f503735e44a3ddec2b837b63847c2
Dylan Warren <duwtgb@mst.edu>
2020-09-17 12:02:49 -0500

    no more server roles.

--- src/bot.ts
+++ src/bot.ts
@@ -19,7 +19,6 @@ interface Command {
 export default class SetsuBot extends Discord.Client {
   config: any;
   commands: Discord.Collection<string, Command>;
-  reactMessages: string[];
   bannedWords: RegExp[];
   bannedStrings: { id: string, word: string }[];
   mutes: Discord.Collection<string, NodeJS.Timeout>;
@@ -31,7 +30,6 @@ export default class SetsuBot extends Discord.Client {
     this.mutes = new Discord.Collection();
     this.commands = new Discord.Collection();
     this.bannedWords = [];
-    this.reactMessages = [];
     this.bannedStrings = [];
     commandHandler(this);
     this.once('ready', () => {
@@ -53,8 +51,6 @@ export default class SetsuBot extends Discord.Client {
         !message.member?.hasPermission('MANAGE_MESSAGES')
       ) {
         this.filterWords(message as Discord.Message);
-      } else if (message.channel?.type === 'dm') {
-        this.serverRoles(message as Discord.Message);
       }
 
       return;
@@ -132,59 +128,6 @@ export default class SetsuBot extends Discord.Client {
     // Verify user into the server
     return member.roles.add(config.STUDENT_ROLE)
       .catch(() => console.error(`Issues verifying member.`));
-  }
-
-  serverRoles = async (message: Discord.Message) => {
-    const words = message.content.split(' ').join('').toLowerCase();
-    const guild = this.guilds.cache.get(config.GUILD);
-    if(!guild) return;
-    let member = await guild.members.cache.get(message.author.id);
-    if (!member) {
-      console.log(`Failed to get member from cache for DMs. Going to fetch and retry....`);
-      await guild.members.fetch(message.author.id);
-      member = guild.members.cache.get(message.author.id);
-    }
-
-    if (!member) 
-      return message.reply(`Sorry! I had issues getting you from the LoveLetter server. Please alert a mod of admin of this.`);
-
-    if (
-      (words.startsWith('bbgame') || words.startsWith('bbserver')) &&
-      !member.roles.cache.has(config.STUDENT_ROLE)
-    ) {
-      return message.reply(`You need to verify on the server first before getting other roles. Check out <#741102101618229319>`);
-    }
-
-    /**
-     * Because people are stupid and can't read just give them roles if the start 
-     * of their message has any of these. Sigh.
-     */
-
-    if (words.startsWith('bbgame')) {
-      // Give game updates role
-      if (!member.roles.cache.has(config.GAME_ROLE)) {
-        member.roles.add(config.GAME_ROLE)
-          .then(() => message.reply(`Congrats! You will now be updated about the game!`))
-          .catch(() => message.reply(`I encountered an issue, show this to a mod or admin to add the game role.`));
-      } else {
-        member.roles.remove(config.GAME_ROLE)
-          .then(() => message.reply(`I removed the game updates role for you.`))
-          .catch(() => message.reply(`I encountered an issue, show this to a mod or admin to remove the game role.`));
-      }
-    } else if (words.startsWith('bbserver')) {
-      // Give server updates role
-      if (!member.roles.cache.has(config.SERVER_ROLE)) {
-        member.roles.add(config.SERVER_ROLE)
-          .then(() => message.reply(`Congrats! You will now be updated about the server!`))
-          .catch(() => message.reply(`I encountered an issue, show this to a mod or admin to add the server role.`));
-      } else {
-        member.roles.remove(config.SERVER_ROLE)
-          .then(() => message.reply(`I removed the server updates role for you.`))
-          .catch(() => message.reply(`I encountered an issue, show this to a mod or admin to remove the server role.`));
-      }
-    }
-
-    return;
   }
 
   filterWords = async (message: Discord.Message) => {
