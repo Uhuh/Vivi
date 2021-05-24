7796288c1c333f5f1ce3ff313a561230a4c9861c
Dylan Warren <duwtgb@mst.edu>
2021-05-23 20:28:36 -0500

    Rewording for banwords listing

---
+++ commands/config/banWordList.ts
@@ -0,0 +1,22 @@
+import { Message } from 'discord.js';
+import { GET_BANNED_WORDS } from '../../src/database/database';
+
+export const listWords = {
+  desc: 'List of currently banned words.',
+  name: 'banwordlist',
+  args: '',
+  alias: ['bwl'],
+  type: 'config',
+  run: async (message: Message) => {
+    if (!message.guild || !message.member?.hasPermission(['MANAGE_GUILD']))
+      return;
+
+    const words = await GET_BANNED_WORDS(message.guild.id);
+
+    message.channel.send(
+      !words.length
+        ? `There are no banned words.`
+        : `Banned words: ||${words.join(', ')}||`
+    );
+  },
+};
