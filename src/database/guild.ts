e0b862313d7743eb3070ed361244d4c2e4aa1ee4
Dylan Warren <dylanwarren19@gmail.com>
2021-11-03 10:28:57 -0500

    Renamed file, removed warn id

---
+++ src/database/guild.ts
@@ -0,0 +1,56 @@
+import { Schema, Document, Model, model } from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import * as config from '../vars';
+
+const GuildConfig = new Schema({
+  guildId: { type: String, required: true, unique: true, index: true },
+  maxWarns: { type: Number, min: 1, default: 3 },
+  // This will be set in days.
+  warnLifeSpan: { type: Number, min: 1, default: 7 },
+  prefix: { type: String, default: 'v.' },
+  modLog: { type: String, default: null },
+  serverLog: { type: String, default: null },
+  serverLogWhitelist: { type: [String], default: [] },
+  banMessage: { type: String, default: null, maxlength: 1020 },
+  bannedWords: {
+    type: [String],
+    default: [...config.DEFAULT_BANNED.split(' ')],
+    maxlength: 120,
+  },
+  joinRoles: {
+    type: [String],
+    default: [],
+    maxlength: 5,
+  },
+  muteRole: { type: String, default: null },
+  modRole: { type: String, default: null },
+  nextCaseId: { type: Number, default: 1 },
+  nextWarnId: { type: Number, default: 1 },
+  bannerType: { type: String, default: 'left' },
+  welcomeChannel: { type: String, default: null },
+});
+
+// type LoggingType = 'deleted_msg' | 'edit_msg' | 'user_join' | 'user_leave' | 'invite_created' | 'invite_used' |
+
+export interface IGuildConfig {
+  guildId: string;
+  maxWarns?: number;
+  warnLifeSpan?: number;
+  prefix?: string;
+  modLog?: string;
+  serverLog?: string;
+  serverLogWhitelist?: string[];
+  banMessage?: string;
+  bannedWords?: string[];
+  joinRoles?: string[];
+  muteRole?: string;
+  modRole?: string;
+  nextCaseId?: number;
+  bannerType?: 'left' | 'center';
+  welcomeChannel?: string;
+}
+
+export interface IGuildConfigDoc extends IGuildConfig, Document {}
+export interface IGuildConfigModel extends Model<IGuildConfigDoc> {}
+export default model<IGuildConfigDoc>('GuildConfig', GuildConfig);
