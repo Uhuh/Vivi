a3b25ea48b658500b96f98ee1a0783d1cade6f3a
Dylan Warren <dylanwarren19@gmail.com>
2021-11-06 00:20:41 -0500

    I need to convert the dates they were given

--- src/database/migrations/migrate_warns_to_cases.ts
+++ src/database/migrations/migrate_warns_to_cases.ts
@@ -1,5 +1,6 @@
 import WarnModel from '../warnings';
 import CaseModel from '../cases';
+import * as moment from 'moment';
 
 export async function migrate_warns_to_cases(guildId: string) {
   const warns = await WarnModel.find({ guildId });
@@ -10,6 +11,7 @@ export async function migrate_warns_to_cases(guildId: string) {
       { guildId: warn.guildId, warnId: warn.warnId },
       {
         reason: warn.reason,
+        creationDate: moment(warn.date).toDate(),
       }
     );
   }
