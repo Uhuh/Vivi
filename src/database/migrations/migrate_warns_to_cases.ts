71e39d2837c1b45714ae4869d8b8669ff6c329de
Dylan Warren <dylanwarren19@gmail.com>
2021-11-06 02:34:16 -0500

    lol

--- src/database/migrations/migrate_warns_to_cases.ts
+++ src/database/migrations/migrate_warns_to_cases.ts
@@ -5,7 +5,10 @@ import * as moment from 'moment';
 export async function migrate_warns_to_cases(guildId: string) {
   const warns = await WarnModel.find({ guildId: Number(guildId) });
 
+  console.log(`==================== WARN ====================`);
   for (const warn of warns) {
+    console.log(warn);
+
     await CaseModel.findOneAndUpdate(
       { guildId: guildId, warnId: warn.warnId },
       {
@@ -14,6 +17,7 @@ export async function migrate_warns_to_cases(guildId: string) {
       }
     ).exec();
   }
+  console.log(`==================== UPDATED CASES ====================`);
   const updatedCases = await CaseModel.find({ guildId, type: CaseType.warn });
   console.log(updatedCases);
 }
